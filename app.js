87114013c2704a41b0483c84ceba143f575ffd5e
KT5334 <yfq5334@autuni.ac.nz>
2023-08-13 19:59:25 +1200

    configured for Vercel v0.32

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.listen(port, () => console.log(`Server running on port ${port}`));
 
 if (process.env.NODE_ENV === "production") {
     const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.use(express.static(path.resolve(__dirname, 'mern_a_to_z_client', 'build')));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+        res.sendFile(path.resolve(__dirname, 'mern_a_to_z_client', 'build', 'index.html'),function (err) {
             if(err) {
                 res.status(500).send(err)
             }
